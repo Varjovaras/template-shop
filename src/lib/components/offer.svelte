d4d1496185e4525e047638542a0430aacd44f40b
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-08-21 14:22:37 +0300

    uncomponent front page

--- src/lib/components/offer.svelte
+++
@@ -1,5 +0,0 @@
-<div class="container bg-slate-300 shadow-lg rounded-lg p-8 m-4 space-y-4">
-    <h2 class="h1 text-gray-800 font-bold text-lg">What we offer</h2>
-    <p class="text-gray-800 text-sm">ali ali :D</p>
-    <p class="text-gray-800 text-sm">ali ali :D</p>
-</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,28 @@
 <script lang="ts">
-    import Offer from "$lib/components/offer.svelte";
-    import Techonologies from "$lib/components/techonologies.svelte";
 </script>
 
 <div class="px-16 p-4">
-    <div>
-        <Offer />
+    <div class="container bg-slate-300 shadow-lg rounded-lg p-8 m-4 space-y-4">
+        <h2 class="h1 text-gray-800 font-bold text-lg">What we offer</h2>
+        <p class="text-gray-800 text-sm">ali ali :D</p>
+        <p class="text-gray-800 text-sm">ali ali :D</p>
     </div>
 
-    <div>
-        <Techonologies />
-    </div>
-
-    <div class="container flex p-8">
-        <div class="w-2/3">
-            <p>wtf :D</p>
-            <p>:D:D</p>
-        </div>
-        <div>
-            <p>wtf :D</p>
-            <p>!??!?!</p>
-        </div>
+    <div class="container shadow-lg rounded-lg border p-8 space-y-4 m-4">
+        <h1 class="h1">Technologies used</h1>
+        <section class="space-y-2">
+            <a class="btn variant-filled-primary" href="https://kit.svelte.dev/"
+                >SvelteKit</a
+            >
+            <a
+                class="btn variant-filled-secondary"
+                href="https://tailwindcss.com/">Tailwind</a
+            >
+            <a
+                class="btn variant-filled-tertiary"
+                href="https://github.com/Varjovaras/template-shop"
+                >GitHub repository</a
+            >
+        </section>
     </div>
 </div>
