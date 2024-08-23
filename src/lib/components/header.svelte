d1c26fb5f5972b780742ed38f283abf95c427c41
Kristjan Rajaniemi <kristjan.rajaniemi@live.fi>
2024-08-23 16:45:58 +0300

    add login button for google oauth

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,9 +1,16 @@
 <script lang="ts">
+	import { supabase } from '$lib/supabaseClient';
+
+	const fnfn = () => {
+		supabase.auth.signInWithOAuth({
+			provider: 'google',
+		});
+	};
 </script>
 
 <header
-    class="bg-surface-500/15 shadow-lg p-4 flex items-center justify-between w-full"
+	class="bg-surface-500/15 shadow-lg p-4 flex items-center justify-between w-full"
 >
-    <h1 class="h2">Website for noobs</h1>
-    <div>login</div>
+	<h1 class="h2">Website for noobs</h1>
+	<button on:click={fnfn}>login</button>
 </header>
